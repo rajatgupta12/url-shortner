b976bac7741dd76431fde2a6a6ebbecb4773dbea
rajatgupta12 <gupta28rajat@gmail.com>
2026-01-12 13:15:35 +0530

    integrated rate limiter

--- src/main/java/com/example/urlshortner/controller/UrlMappingController.java
+++ src/main/java/com/example/urlshortner/controller/UrlMappingController.java
@@ -3,7 +3,10 @@ package com.example.urlshortner.controller;
 import com.example.urlshortner.dto.response.BaseResponse;
 import com.example.urlshortner.dto.response.UrlMappingResponse;
 import com.example.urlshortner.dto.request.UrlRequest;
+import com.example.urlshortner.exception.UrlShortnerException;
 import com.example.urlshortner.service.UrlMappingService;
+import io.github.resilience4j.ratelimiter.RequestNotPermitted;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.swagger.v3.oas.annotations.Hidden;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,9 +23,14 @@ public class UrlMappingController {
     private final UrlMappingService urlMappingService;
 
     @PostMapping("/url")
+    @RateLimiter(name = "apiLimiter", fallbackMethod = "rateLimitFallback")
     public BaseResponse<UrlMappingResponse> createURLMapping(@RequestBody UrlRequest urlRequest, Authentication authentication) {
         UrlMappingResponse response = urlMappingService.createUrlMapping(urlRequest.getUrl(), authentication.getName());
         return new BaseResponse<>(response);
+    }
+
+    public BaseResponse<UrlMappingResponse> rateLimitFallback(UrlRequest urlRequest, Authentication authentication, RequestNotPermitted ex) {
+        throw new UrlShortnerException("Too many requests", HttpStatus.TOO_MANY_REQUESTS);
     }
 
     @Hidden
